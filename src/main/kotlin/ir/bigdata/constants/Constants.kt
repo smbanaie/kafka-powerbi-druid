582332571d5a5616995d815a3cb7bd2e3f91f4ca
S.Mojtaba Banaie <mojtaba.banaie@gmail.com>
2019-01-17 22:22:53 +0330

    First Commit

---
+++ src/main/kotlin/ir/bigdata/constants/Constants.kt
@@ -0,0 +1,17 @@
+package ir.bigdata.constants
+
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.util.StdDateFormat
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+
+val personsTopic = "persons2"
+val personsAvroTopic = "persons-avro"
+val agesTopic = "ages"
+
+val jsonMapper = ObjectMapper().apply {
+    registerKotlinModule()
+    disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    setDateFormat(StdDateFormat())
+}
---
+++ src/main/kotlin/ir/bigdata/models/Person.kt
@@ -0,0 +1,9 @@
+package ir.bigdata.models
+
+import java.util.*
+
+data class Person(
+    val firstName: String,
+    val lastName: String,
+    val birthDate: Date
+)
